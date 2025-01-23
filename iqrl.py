Fix state normalization when using offline data
Two crucial changes that explain the previously bad results:
1) States are normalized per-task now to avoid messing with zero-padding
2) When sampling actions (in particular in eval), apply normalization

iqrl.py
@@ -88,6 +88,8 @@ class iQRLConfig:
     fsq_levels: List[int] = field(default_factory=lambda: [8, 8])
     """Use offline data to train and use TD3-BC instead of TD3"""
     use_offline_data: bool = "${use_offline_data}"  # Set from TrainConfig
+    """States are normalized per-task"""
+    normalize_states: bool = "${normalize_states}"  # Set from TrainConfig
     """When using offline data, this is the only additional parameter (see TD3-BC)"""
     bc_alpha: float = 2.5
 
@@ -515,6 +517,8 @@ class iQRL(nn.Module):
         self.critic_update_counter = 0
         self.pi_update_counter = 0
 
+        self.state_norm = None  # If cfg.normalize_states==True, will be set later
+
     def update(self, replay_buffer: ReplayBuffer, num_new_transitions: int) -> dict:
         """Update representation and TD3 at same time"""
         num_updates = int(num_new_transitions * self.cfg.utd_ratio)
@@ -723,15 +727,36 @@ class iQRL(nn.Module):
             "actor_update_counter": self.pi_update_counter,
         }
 
+    def set_state_norm(self, state_norm: dict):
+        self.state_norm = state_norm  # (body_id, task_id)->(mean, std)
+
     @torch.no_grad()
-    def select_action(self, obs: TensorDict, eval_mode: bool = False):
+    def select_action(self, obs: TensorDictBase, eval_mode: bool = False):
+        if self.cfg.normalize_states and self.state_norm is not None:
+            # Normalize states (no need to pad though; this will be done by the encoder)
+            use_nested_tensor = isinstance(obs, LazyStackedTensorDict)
+            if use_nested_tensor:
+                state = obs.get_nestedtensor("state")
+            else:
+                state = obs["state"]
+            normalized_states = []
+            num_states = state.size(0)  # This works for both tensor and nested tensor
+            for i in range(num_states):
+                body_id = np.argmax(obs["body_id"][i]).item()
+                task_id = np.argmax(obs["task_id"][i]).item()
+                mean, std = self.state_norm[(body_id, task_id)]
+                normalized_states.append((state[i] - mean) / std)
+            if use_nested_tensor:
+                obs["state"] = torch.nested.nested_tensor(normalized_states)
+            else:
+                obs["state"] = torch.stack(normalized_states)
+
         is_flat_obs = False
         if obs.batch_size == torch.Size([]):
             obs = obs.view(1)
             is_flat_obs = True
 
         z = self.encoder.encode(obs, tar=False).to(torch.float)
-
         a = self.pi(z["state"], tar=False, eval_mode=eval_mode)
 
         if is_flat_obs:

train.py
@@ -422,6 +422,7 @@ def train(cfg: TrainConfig):
         )
 
         offline_data = []
+        state_normalization = {}  # Needed during eval if cfg.normalize_states==True
         for i in range(env_count):
             MT30_DATA_DIR = os.path.join(
                 os.environ.get("WRKDIR"), "data", "mt30", "per-task"
@@ -441,24 +442,30 @@ def train(cfg: TrainConfig):
             task_data["action"] = task_data_raw["action"][:, 1:]
             task_data["reward"] = task_data_raw["reward"][:, 1:]
             # NOTE: "done" and ("next", "terminated") can remain False all the way, it's fine
+            if cfg.normalize_states:
+                # Normalize over all states of this specific body&task combination
+                # States shape is (episode_count, ep_length, max_state_dim) -> dim=(0, 1)
+                mean = task_data["observation"]["state"].mean(dim=(0, 1))
+                std = task_data["observation"]["state"].std(dim=(0, 1)) + 1e-3
+                task_data["observation"]["state"] = (
+                    task_data["observation"]["state"] - mean
+                ) / std
+                task_data["next"]["observation"]["state"] = (
+                    task_data["next"]["observation"]["state"] - mean
+                ) / std
+
+                body_id = np.argmax(body_str_to_id[cfg.envs[i][0]]).item()
+                task_id = np.argmax(task_str_to_id[cfg.envs[i][1]]).item()
+                state_normalization[(body_id, task_id)] = (mean, std)
 
             offline_data.append(task_data)
+
+        if cfg.normalize_states:
+            agent.set_state_norm(state_normalization)
 
         data = LazyStackedTensorDict.lazy_stack(offline_data, dim=0)
         data = pad_sequence(data, pad_dim=-1)  # LazyStackedTensorDict -> TensorDict
         data = data.flatten(0, 1)  # Merge first 2 dims: env_count and episode_count
-
-        if cfg.normalize_states:
-            # Normalize over all states (even across different tasks)
-            eps = 1e-3
-            # States have shape: (episode_count, ep_length, max_state_dim)
-            mean = data["observation"]["state"].mean(dim=(0, 1), keepdims=True)
-            std = data["observation"]["state"].std(dim=(0, 1), keepdims=True) + eps
-            data["observation"]["state"] = (data["observation"]["state"] - mean) / std
-            data["next"]["observation"]["state"] = (
-                data["next"]["observation"]["state"] - mean
-            ) / std
-            print(f"Normalized over all states:\n\tmean={mean}\n\tstd={std})")
 
         rb.extend(data)
 